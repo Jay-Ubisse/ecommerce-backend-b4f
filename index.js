91decd858e3500dcf1ec6ff971d6e15330c690dc
Jay-Ubisse <joaquimubisse@gmail.com>
2024-07-19 15:59:11 +0200

    feat: project structure

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello Madrinha");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use("/api/users", require("./routes/user-route"));
+app.use("/api/products", require("./routes/product-route"));
+app.use("/api/orders", require("./routes/order-route"));
+app.use("/api/customer", require("./routes/customer-route"));
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
