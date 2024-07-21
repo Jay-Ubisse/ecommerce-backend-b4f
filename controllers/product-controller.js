db6a01ad40be2ef70f9c95c03b977b0e10ba001d
Djaysambo <vicentesambo3@gmail.com>
2024-07-21 21:44:32 +0200

    feat: read product

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,6 +1,14 @@
 
 
 
+readProducts.get('/:id', (req, res) => {
+  const productId = parseInt(req.params.id);
+  const product = products.find(product => product.id === productId);
+  if (!product) {
+      return res.status(404).send('Product not found');
+  }
+  res.json(product);
+});
 
 const deleteProducts('/:id') = (req, res) => {
   res.status(200).json({ message: `Eliminar um producto com id ${req.params.id}` });
