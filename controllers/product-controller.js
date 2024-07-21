e5d52ae0752d10c151ab909e4193fc5821e83ad6
Domingos chiconela <azevedodomingos78@gmail.com>
2024-07-21 22:08:12 +0200

    feat:created routers to create, update products and their controllers

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,7 +1,26 @@
 
 
 
+const createProduct = (req,res)=>{
+    const {name,category,price,description} = req.body
 
-const deleteProducts('/:id') = (req, res) => {
+    req.status(201).json({message:'Product created'})
+}
+
+
+const updateProduct =  (req,res)=>{
+   
+    const id = request.params.id
+    const {name,category,price,description} = req.body
+
+    req.status(200).json({message:'Product updated'})
+}
+
+
+const deleteProduct = (req, res) => {
   res.status(200).json({ message: `Eliminar um producto com id ${req.params.id}` });
 };
+
+
+
+module.exports =  {createProduct,updateProduct,deleteProduct}
