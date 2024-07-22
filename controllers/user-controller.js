5769169e662b5407414e6a2c462e0ba8e5a42e85
AtaliaPatissone <ataliadacostapatissone@gmail.com>
2024-07-22 10:25:03 +0200

    Created getUser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,9 @@
 const GetAllUser = (req, res) => {
   req.status(200).json({ message: "Obter todos os Usuarios" });
 };
+
+const getUser = (req, res) => {
+  res.status(200).json({
+    message: `Obter Usuario ${req.params.id}`,
+  });
+};
--- routes/user-route.js
+++ routes/user-route.js
@@ -2,3 +2,5 @@ const express = require("express");
 const { GetAllUser } = require("../controllers/user-controller");
 const router = express.Router();
 router.get("/", GetAllUser);
+
+router.get("/:id", getUser);
