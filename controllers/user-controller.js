bdebab8ab5fb823a80b26c5d40ed22d0ed5c7a2d
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-22 12:52:40 +0200

    add Delete user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,4 +10,8 @@ const getUser = (req, res) => {
 const createUser = (req, res) => {
   res.status(201).json({ message: "Usuario criado com sucesso!", data: req.body });
 };
-module.exports = { getAllUser, getUser,  createUser};
+
+const deleteUser = (req,res) =>{
+  res.status(200).json({message:`Usuario Eliminado ${req.params.id}`});
+}
+module.exports = { getAllUser, getUser,  createUser,deleteUser};
--- routes/user-route.js
+++ routes/user-route.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getAllUser,getUser ,createUser,} = require("../controllers/user-controller");
+const { getAllUser,getUser ,createUser,deleteUser} = require("../controllers/user-controller");
 
 
 
@@ -12,4 +12,5 @@ router.get("/:id", getUser);
 
 router.post("/", createUser);
 
+router.delete("/:id",deleteUser)
 module.exports = router;
