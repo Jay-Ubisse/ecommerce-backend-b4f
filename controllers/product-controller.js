6e33f3f9dc94340f3c5efd0038b85e546ba86472
ITSimaoAbel <simao3cuco@gmail.com>
2024-07-25 16:20:52 +0200

    Add: creating product

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -15,11 +15,21 @@ const getProduct = (req, res) => {
 };
 
 
-const createProduct = (req, res) => {
-  const { name, category, price, description } = req.body
+const createProduct = asyncHandler(async(req, res) => {
+  const createProduct = await Product.create({
+    title: req.body.title,
+    name: req.body.name,
+    price: req.body.price,
+    category: req.body.category,
+    quantityStock: req.body.quantityStock,
+    description: req.body.description,
+  });
 
-  req.status(201).json({ message: 'Product created' })
-};
+  res.status(200).json({
+    message: "Producto criado com sucesso!",
+    data: createProduct,
+  });
+});
 
 
 const updateProduct = (req, res) => {
