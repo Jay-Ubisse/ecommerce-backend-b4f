ddd565dc0b11b4424fcc1623a85c48cd64417f35
Domingos chiconela <azevedodomingos78@gmail.com>
2024-07-25 15:39:55 +0200

    fix:conection

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,46 +1,28 @@
-const asyncHandler = require ("express-async-handler");
-const Product = require("../models/produts-model");
 
 
 
-const getAllProduct = asyncHandler(async (req, res) => {
-  const products = await Product.find();
 
-  res.status(200).json({ message: "Todos Produtos", data: products });
-});
+const createProduct = (req,res)=>{
+  const {name,category,price,description} = req.body
 
-
-const getAllProducts = (req, res) => {
-  res.status(200).json({ message: `Todos os productos`})
-};
-
-const getProduct = (req, res) => {
-
-  res.status(200).json({ message: `Produto ${req.params.id}  encontrado` })
-};
+  req.status(201).json({message:'Product created'})
+}
 
 
-const createProduct = (req, res) => {
-  const { name, category, price, description } = req.body
-
-  req.status(201).json({ message: 'Product created' })
-};
-
-
-const updateProduct = (req, res) => {
-
+const updateProduct =  (req,res)=>{
+ 
   const id = request.params.id
-  const { name, category, price, description } = req.body
+  const {name,category,price,description} = req.body
 
-  req.status(200).json({ message: 'Product updated' })
-};
+  req.status(200).json({message:'Product updated'})
+}
 
 
 const deleteProduct = (req, res) => {
-  res.status(200).json({ message: `Eliminar um producto com id ${req.params.id}` });
+res.status(200).json({ message: `Eliminar um producto com id ${req.params.id}` });
 };
 
 
 
 
-module.exports = { createProduct, updateProduct, deleteProduct, getProduct, getAllProducts }
+module.exports =  {createProduct,updateProduct,deleteProduct}
