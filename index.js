959dabe06f31fe179bc35648db533a3f36c6e774
jubisse <jubisse@hotmail.com>
2024-07-25 12:32:46 +0200

    feat: database connection and configuration

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const connectDB = require("./connect/database");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+
+connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.use("/api/users", require("./routes/user-route"));
 app.use("/api/products", require("./routes/product-route"));
 app.use("/api/orders", require("./routes/order-route"));
 app.use("/api/customer", require("./routes/customer-route"));
-
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+app.listen(port, () => console.log(`Aplicação rodando em http://localhost:${port}`));
